38076087b0a452a7d64587912d2e7af506a387dd
OlyaBezrukova <74977261+OlyaBezrukova@users.noreply.github.com>
2020-12-02 15:18:49 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,3 +1,5 @@
 import numpy as np
 list = ['Makar Sidorov 24', 'Maria Kuznetsova 20', 'Reter Petrov 19', 'Ivan Ivanov 21']
 print(sorted(list, key = lambda x: str(x[-2:])))
+a = sorted(list, key = lambda x: str(x[-2:]))
+print(a[0],[1])
